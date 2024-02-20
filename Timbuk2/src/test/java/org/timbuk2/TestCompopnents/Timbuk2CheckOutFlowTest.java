b0d02d6bf6cb2721e00dd951dcb239339b75d58f
Exemplistest <exemplistest123@gmail.com>
2024-02-20 18:45:58 +0530

    changes havebeen made to the checkoutflow test case

--- Timbuk2/src/test/java/org/timbuk2/TestCompopnents/Timbuk2CheckOutFlowTest.java
+++ Timbuk2/src/test/java/org/timbuk2/TestCompopnents/Timbuk2CheckOutFlowTest.java
@@ -17,42 +17,43 @@ import org.timbuk2.connections.ConnectionsClass;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 
+
 public class Timbuk2CheckOutFlowTest extends ConnectionsClass {
 
 	public WebDriver driver;
-
-//	@Test
-//	public void checkOutFlow() throws InterruptedException, IOException, CsvException {
-////		WebDriverManager.edgedriver().setup();
-////		driver = new EdgeDriver();
-////		driver.manage().window().maximize();
-////		Timbuk2LandingPage landingPage = new Timbuk2LandingPage(driver);
-////		landingPage.goTo();
-//		Timbuk2LandingPage landingPage = launchApplication();
-//		landingPage.discountCode("VeniceBeach-Retail-DTA935E9ED-347641589");
-//		Timbuk2ProductPage productPage = landingPage.acceptCookies();
-//		Timbuk2GetProducts getProducts = productPage.selectProduct();
-//		Timbuk2SecureCheckOut secureCheckOut = getProducts.getProductList();
-//		secureCheckOut.secureCheckOut("password");
-//		Timbuk2ReviewAndSubmitOrder reviewAndSubmitOrder = secureCheckOut.secureLogin();
-//		reviewAndSubmitOrder.selectCountry("Canada");
-//		reviewAndSubmitOrder.selectState("Ontario");
-//		CSVReader userAddress = new CSVReader(
-//				new FileReader("C:\\Users\\Nagendra Vellanki\\Desktop\\Timbuk2\\timbuk2address.csv"));
-//		List<String[]> csvData = userAddress.readAll();
-//		for (int i = 1; i < csvData.size(); i++) {
-//			String[] row = csvData.get(i);
-//			String firstName = row[0];
-//			String lastName = row[1];
-//			String email = row[2];
-//			String address = row[3];
-//			String city = row[4];
-//			String postal = row[5];
-//			String phone = row[6];
-//			reviewAndSubmitOrder.enterAddressDetails(firstName, lastName, email, address, city, postal, phone);
-//			reviewAndSubmitOrder.submitAddressDetails();
-//		}
-//	}
+//Changes done for the checkoutflow testcase
+	@Test
+	public void checkOutFlow() throws InterruptedException, IOException, CsvException {
+//		WebDriverManager.edgedriver().setup();
+//		driver = new EdgeDriver();
+//		driver.manage().window().maximize();
+//		Timbuk2LandingPage landingPage = new Timbuk2LandingPage(driver);
+//		landingPage.goTo();
+		Timbuk2LandingPage landingPage = launchApplication();
+		landingPage.discountCode("VeniceBeach-Retail-DTA935E9ED-347641589");
+		Timbuk2ProductPage productPage = landingPage.acceptCookies();
+		Timbuk2GetProducts getProducts = productPage.selectProduct();
+		Timbuk2SecureCheckOut secureCheckOut = getProducts.getProductList();
+		secureCheckOut.secureCheckOut("password");
+		Timbuk2ReviewAndSubmitOrder reviewAndSubmitOrder = secureCheckOut.secureLogin();
+		reviewAndSubmitOrder.selectCountry("Canada");
+		reviewAndSubmitOrder.selectState("Ontario");
+		CSVReader userAddress = new CSVReader(
+				new FileReader("C:\\Users\\Nagendra Vellanki\\Desktop\\Timbuk2\\timbuk2address.csv"));
+		List<String[]> csvData = userAddress.readAll();
+		for (int i = 1; i < csvData.size(); i++) {
+			String[] row = csvData.get(i);
+			String firstName = row[0];
+			String lastName = row[1];
+			String email = row[2];
+			String address = row[3];
+			String city = row[4];
+			String postal = row[5];
+			String phone = row[6];
+			reviewAndSubmitOrder.enterAddressDetails(firstName, lastName, email, address, city, postal, phone);
+			reviewAndSubmitOrder.submitAddressDetails();
+		}
+	}
 	
 
 	@Test
